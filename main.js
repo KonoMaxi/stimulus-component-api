d602940cfca2a3b803d48ee7019c9ebca26b9c67
Maximilian Konowski <m.konowski@itemsnet.de>
2022-09-04 01:46:15 +0200

    building demo

--- main.js
+++ main.js
@@ -4,9 +4,3 @@ import { registerControllers } from 'stimulus-vite-helpers'
 const application = Application.start()
 const controllers = import.meta.globEager('./**/*_controller.js')
 registerControllers(application, controllers)
-
-// import { createApp } from 'vue3'
-// import HelloComponent from './src/demo/vue3/HelloComponent'
-
-// const app = createApp(HelloComponent)
-// app.mount('#app')
