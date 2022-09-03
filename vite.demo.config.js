09a3cbc68466405edee666e8543f1b8203f3ccfa
Maximilian Konowski <m.konowski@itemsnet.de>
2022-09-04 01:40:29 +0200

    added build config for demo

---
+++ vite.demo.config.js
@@ -0,0 +1,10 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte({})],
+  build: { }
+})
